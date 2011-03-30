2e41ecdff237644cb1d6a51aaea223efc2cc41f5
vinks <vin.konst@gmail.com>
2011-03-30 18:53:41 +0400

    comment

--- application/logs/2011/03/29.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-03-29 12:16:31 --- ERROR: ErrorException [ 2 ]: array_fill(): Number of elements must be positive ~ MODPATH/orm/classes/kohana/orm.php [ 989 ]
-2011-03-29 12:16:37 --- ERROR: ErrorException [ 2 ]: array_fill(): Number of elements must be positive ~ MODPATH/orm/classes/kohana/orm.php [ 989 ]
-2011-03-29 12:18:57 --- ERROR: Database_Exception [ 0 ]: pg_connect(): Unable to connect to PostgreSQL server: could not connect to server: Connection timed out
-	Is the server running on host "192.168.0.159" and accepting
-	TCP/IP connections on port 5432? ~ MODPATH/database/classes/kohana/database/postgresql.php [ 55 ]
-2011-03-29 12:56:26 --- ERROR: ReflectionException [ -1 ]: Class controller_admin does not exist ~ SYSPATH/classes/kohana/request.php [ 1199 ]
-2011-03-29 12:56:30 --- ERROR: ReflectionException [ -1 ]: Class controller_admin does not exist ~ SYSPATH/classes/kohana/request.php [ 1199 ]
